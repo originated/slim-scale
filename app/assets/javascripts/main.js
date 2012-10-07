5f0ab90ade167368e42c6174e79980d4851d9273
Mark Maximus Muskardin <mark.muskardin@gmail.com>
2012-10-07 12:18:47 -0700

    js fixes oh yeaaaa

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,23 +1,26 @@
-SlimScale = {
-	init:function(){
-		var self = this;
-		self.trackWeightButton();
-	},
-	trackWeightButton: function(){
-		$('#track-weight').click(function(){
-			$.ajax({
-                url:'/mike_controller',
-                dataType: 'json',
-                type: 'get',
-                success:function (resArr) {
-                    // convert response string to array, merge with billBoards arr, then pass to callback
-                },
-				error: function(res){
-					console.log('boohooo! ajax error');
-				}
-            });
-		});
+$(document).ready(function() {
+	SlimScale = {
+		init:function(){
+			var self = this;
+			self.trackWeightButton();
+		},
+		trackWeightButton: function(){
+			$('#track-weight').click(function(){
+				$.ajax({
+	                url:'/mike_controller',
+	                dataType: 'json',
+	                type: 'get',
+	                success:function (resArr) {
+	                    // convert response string to array, merge with billBoards arr, then pass to callback
+	                },
+					error: function(res){
+						console.log('boohooo! ajax error');
+					}
+	            });
+			});
+		}
 	}
-}
 
-SlimScale.init();
+	SlimScale.init();
+});
+
